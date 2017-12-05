ebc78fb8bc7aad1e416f77430aa84f9d8acb55f1
manishkm <manishkm@dewsolutions.in>
2017-12-05 18:49:09 +0530

    change

--- angular/UIConfig.js
+++ angular/UIConfig.js
@@ -4,7 +4,7 @@ angular.module('UIConfig', [])
 	// webservice_domain : 'http://localhost:9090/ImageVideoApp',
 	webservice_domain : 'http://205.147.101.198:8080/ImageVideoApp',
 
-	app_domain : 'http://205.147.101.198/#/'
+	app_domain : 'http://showofff.com/#/'
 	})
 
 ;
