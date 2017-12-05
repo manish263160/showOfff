ed13d824e8345c898c423087e7abefc8322ebf53
manishkm <manishkm@dewsolutions.in>
2017-12-05 20:05:27 +0530

    change in about us

--- angular/controller/aboutusController.js
+++ angular/controller/aboutusController.js
@@ -2,6 +2,45 @@ app
 .controller("aboutusController",
 function ($scope, $http, $window, $rootScope, $timeout, Service ,$interval) {
 
+    jQuery(document).ready(function ($) {
+        
+                    var jssor_1_options = {
+        
+                      $AutoPlay: 1,
+        
+                      $DragOrientation: 2,
+        
+                      $PlayOrientation: 2,
+        
+                      $ArrowNavigatorOptions: {
+        
+                        $Class: $JssorArrowNavigator$
+                      }
+                    };
+        
+                    var jssor_1_slider = new $JssorSlider$("jssor_1", jssor_1_options);
+        
+                    /*responsive code begin*/
+                    /*remove responsive code if you don't want the slider scales while window resizing*/
+                    function ScaleSlider() {
+        
+                        var refSize = jssor_1_slider.$Elmt.parentNode.clientWidth;
+        
+                        if (refSize) {
+        
+                            refSize = Math.min(refSize, 600);
+        
+                            jssor_1_slider.$ScaleWidth(refSize);
+                        }
+                        else {
+        
+                            window.setTimeout(ScaleSlider, 30);
+                        }
+                    }
+        
+        
+                });
+
     // Vertical Scroll TOP
  $(function () {
     $('#head_top').bind("click", function () {
